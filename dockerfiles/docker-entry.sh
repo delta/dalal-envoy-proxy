4c2424413f406fee051a8cd4c9dbe1f53ef39ae4
siva2204 <sivaraman26r@gmail.com>
2021-11-11 23:12:37 +0530

    [Add][dev] dockerize

---
+++ dockerfiles/docker-entry.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ -f .env ]
+then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+
+sed "s/DALAL_BACKEND_HOST/$DALAL_BACKEND_HOST/g; s/DALAL_BACKEND_PORT/$DALAL_BACKEND_PORT/g" dev.envoy.yaml > envoy.yaml
